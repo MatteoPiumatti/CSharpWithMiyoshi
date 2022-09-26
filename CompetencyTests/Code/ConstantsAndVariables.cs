a568dd6422051c648a4357a50ae9e025d2107ae6
MatteoPiumatti <113454523+MatteoPiumatti@users.noreply.github.com>
2022-09-26 14:12:59 -0700

    made it have the abillity to display the text

--- CompetencyTests/Code/ConstantsAndVariables.cs
+++ CompetencyTests/Code/ConstantsAndVariables.cs
@@ -23,6 +23,13 @@ int main()
     const double p = 3.1415; //Pi= the realationship between the radius and the circumfrance.
     const double k = 1.38e-23; //Boltzman constant relates the enrgy and temperature of a gas.
     bool test = true;
+    {
+        Console.WriteLine("speed of light",c);
+        Console.WriteLine("acc of gravity",g);
+        Console.WriteLine("planks constant",h);
+        Console.WriteLine("Pi",p);
+        Console.WriteLine("boltzman constant",k);
+    }
     if (test == true)
     {
         Console.WriteLine("test is true");
@@ -31,6 +38,11 @@ int main()
     int myAge = 14;
     int myHieght = 165; //cm
     int myWeight = 140; //lbs
+    {
+        Console.WriteLine("my age",myAge);
+        Console.WriteLine("my hieght in cm",myHieght);
+        Console.WriteLine("my weight in pounds",myWeight);
+    }
 
     char firstLetterOfMyName = 'M';
     char firstLetterOfLastName = 'P';
@@ -38,9 +50,14 @@ int main()
 
     Console.WriteLine("my initials {0}{1}{2}",firstLetterOfMyName,firstLetterOfLastName,dot);
 
+
+
     string favoriteStroke = "freestyle";
     string leastFavoriteStroke = "backstroke";
     string favoritePlaceToVisit = "Hawaii";
+
+    Console.WriteLine("my favorite swimming stroke",favoriteStroke);
+    Console.WriteLine("my least favorite stroke")
 
 
 
